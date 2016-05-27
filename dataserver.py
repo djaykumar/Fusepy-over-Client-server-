96f8a84b7b987a5e040b8fc101747d48d393880c
djaykumar <dhananjayvkumar@gmail.com>
2016-05-27 16:09:46 +0000

    Add files via upload

---
+++ dataserver.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+"""
+Author: David Wolinsky
+Version: 0.02
+
+Description:
+The XmlRpc API for this library is:
+  get(base64 key)
+    Returns the value and ttl associated with the given key using a dictionary
+      or an empty dictionary if there is no matching key
+    Example usage:
+      rv = rpc.get(Binary("key"))
+      print rv => {"value": Binary, "ttl": 1000}
+      print rv["value"].data => "value"
+  put(base64 key, base64 value, int ttl)
+    Inserts the key / value pair into the hashtable, using the same key will
+      over-write existing values
+    Example usage:  rpc.put(Binary("key"), Binary("value"), 1000)
+  print_content()
+    Print the contents of the HT
+  read_file(string filename)
+    Store the contents of the Hahelperable into a file
+  write_file(string filename)
+    Load the contents of the file into the Hahelperable
+"""
+
+import sys, SimpleXMLRPCServer, getopt, pickle, time, threading, xmlrpclib, unittest
+from datetime import datetime, timedelta
+from xmlrpclib import Binary
+from collections import Counter
+quit = 0
+
+# Presents a HT interface
+class SimpleHT:
+
+  def __init__(self):
+    self.data = {}
+    self.next_check = datetime.now() + timedelta(minutes = 5)
+
+  def count(self):
+    # Remove expired entries
+    self.next_check = datetime.now() - timedelta(minutes = 5)
+    self.check()
+    return len(self.data)
+
+
+  def sync_rec(self, key):
+    res_d=[]
+    for x in range(2,len(sys.argv)):
+      dataserver_url=[]
+      url = 'http://localhost:'+ sys.argv[x]
+      dataserver_url.append(url) 
+      print 'url is', url
+      rpc = xmlrpclib.Server(url)
+      temp = rpc.get(key)
+      print 'TEMP:',temp['value'].data
+      if 'value' in temp:
+        append_value =temp['value'].data
+        print "RESD:",res_d
+        res_d.append(append_value)
+
+      else: 
+        res_d.append('NULL')
+    k=Counter(res_d)
+    res= max(k,key=k.get)
+    print "RECORD:",res,k
+    self.put(key,Binary(res),6000)
+    print 'Record Sync Successful'
+    return 1
+ 
+  def sync(self):
+    #synchronize with other servers
+    url = 'http://localhost:'+sys.argv[2]
+    print 'url is', url
+    rpc = xmlrpclib.Server(url)
+    res_sync = rpc.list_contents()
+    print 'Keys are: ', res_sync
+    
+    for x in res_sync:
+     # self.put(x,rpc.get(x)["value"].data,6000)
+     temp = rpc.get(Binary(x))
+     print 'First value: ', pickle.loads(temp["value"].data)
+     self.put(Binary(x),Binary(temp["value"].data),6000)
+    print 'Sync Successful'
+    return 1
+
+  def list_contents(self):
+     return self.data.keys()
+  
+  def corrupt(self, key):
+    # Corrupting an entry
+    self.put(key,Binary(pickle.dumps('CORRUPT DATA')),6000)
+    print 'CORRUPTION INJECTED'
+    temp = self.get(key)
+    return temp
+
+  def terminate(self):
+    global quit
+    quit = 1
+    #server = MyServer(('127.0.0.1', 5001))
+    return 1
+
+  # Retrieve something from the HT
+  def get(self, key):
+    # Remove expired entries
+    self.check()
+    # Default return value
+    rv = {}
+    # If the key is in the data structure, return properly formated results
+    key = key.data
+    if key in self.data:
+      ent = self.data[key]
+      print ent[0]
+      now = datetime.now()
+      if ent[1] > now:
+        ttl = (ent[1] - now).seconds
+        rv = {"value": Binary(ent[0]), "ttl": ttl}
+      else:
+        del self.data[key]
+    return rv
+
+  # Insert something into the HT
+  def put(self, key, value, ttl):
+    # Remove expired entries
+    self.check()
+    end = datetime.now() + timedelta(seconds = ttl)
+    print "put value is", value.data
+    self.data[key.data] = (value.data, end)
+    print "dictionary is", self.data
+    return True
+    
+  # Load contents from a file
+  def read_file(self, filename):
+    f = open(filename.data, "rb")
+    self.data = pickle.load(f)
+    f.close()
+    return True
+
+  # Write contents to a file
+  def write_file(self, filename):
+    
+    f = open(filename.data, "wb")
+    pickle.dump(self.data, f)
+    f.close()
+    return True
+
+  # Print the contents of the hashtable
+  def print_content(self):
+    print self.data
+    return True
+
+  # Remove expired entries
+  def check(self):
+    now = datetime.now()
+    if self.next_check > now:
+      return
+    self.next_check = datetime.now() + timedelta(minutes = 5)
+    to_remove = []
+    for key, value in self.data.items():
+      if value[1] < now:
+        to_remove.append(key)
+    for key in to_remove:
+      del self.data[key]
+       
+def main():
+  optlist, args = getopt.getopt(sys.argv[1:], "", ["port=", "test"])
+  ol={}
+  for k,v in optlist:
+    ol[k] = v
+  arg1=sys.argv[1]
+  print 'Server in port', arg1
+  port = int(arg1)
+  if "--port" in ol:
+    port = int(ol["--port"])  
+  if "--test" in ol:
+    sys.argv.remove("--test")
+    unittest.main()
+    return
+  serve(port)
+
+# Start the xmlrpc server
+def serve(port):
+  file_server = SimpleXMLRPCServer.SimpleXMLRPCServer(('', port))
+  file_server.register_introspection_functions()
+  sht = SimpleHT()
+  file_server.register_function(sht.get)
+  file_server.register_function(sht.put)
+  file_server.register_function(sht.sync)
+  file_server.register_function(sht.sync_rec)
+  file_server.register_function(sht.corrupt)
+  file_server.register_function(sht.list_contents)
+  file_server.register_function(sht.terminate)
+  file_server.register_function(sht.print_content)
+  file_server.register_function(sht.read_file)
+  file_server.register_function(sht.write_file)
+  while not quit:
+    file_server.handle_request()
+  #file_server.serve_forever()
+
+# Execute the xmlrpc in a thread ... needed for testing
+class serve_thread:
+  def __call__(self, port):
+    serve(port)
+
+# Wrapper functions so the tests don't need to be concerned about Binary blobs
+class Helper:
+  def __init__(self, caller):
+    self.caller = caller
+
+  def put(self, key, val, ttl):
+    return self.caller.put(Binary(key), Binary(val), ttl)
+
+  def get(self, key):
+    return self.caller.get(Binary(key))
+
+  def write_file(self, filename):
+    return self.caller.write_file(Binary(filename))
+
+  def read_file(self, filename):
+    return self.caller.read_file(Binary(filename))
+
+class SimpleHTTest(unittest.TestCase):
+  def test_direct(self):
+    helper = Helper(SimpleHT())
+    self.assertEqual(helper.get("test"), {}, "DHT isn't empty")
+    self.assertTrue(helper.put("test", "test", 10000), "Failed to put")
+    self.assertEqual(helper.get("test")["value"], "test", "Failed to perform single get")
+    self.assertTrue(helper.put("test", "test0", 10000), "Failed to put")
+    self.assertEqual(helper.get("test")["value"], "test0", "Failed to perform overwrite")
+    self.assertTrue(helper.put("test", "test1", 2), "Failed to put" )
+    self.assertEqual(helper.get("test")["value"], "test1", "Failed to perform overwrite")
+    time.sleep(2)
+    self.assertEqual(helper.get("test"), {}, "Failed expire")
+    self.assertTrue(helper.put("test", "test2", 20000))
+    self.assertEqual(helper.get("test")["value"], "test2", "Store new value")
+
+    helper.write_file("test")
+    helper = Helper(SimpleHT())
+
+    self.assertEqual(helper.get("test"), {}, "DHT isn't empty")
+    helper.read_file("test")
+    self.assertEqual(helper.get("test")["value"], "test2", "Load unsuccessful!")
+    self.assertTrue(helper.put("some_other_key", "some_value", 10000))
+    self.assertEqual(helper.get("some_other_key")["value"], "some_value", "Different keys")
+    self.assertEqual(helper.get("test")["value"], "test2", "Verify contents")
+
+  # Test via RPC
+  def test_xmlrpc(self):
+    output_thread = threading.Thread(target=serve_thread(), args=(51234, ))
+    output_thread.setDaemon(True)
+    output_thread.start()
+
+    time.sleep(1)
+    helper = Helper(xmlrpclib.Server("http://127.0.0.1:51234"))
+    self.assertEqual(helper.get("test"), {}, "DHT isn't empty")
+    self.assertTrue(helper.put("test", "test", 10000), "Failed to put")
+    self.assertEqual(helper.get("test")["value"], "test", "Failed to perform single get")
+    self.assertTrue(helper.put("test", "test0", 10000), "Failed to put")
+    self.assertEqual(helper.get("test")["value"], "test0", "Failed to perform overwrite")
+    self.assertTrue(helper.put("test", "test1", 2), "Failed to put" )
+    self.assertEqual(helper.get("test")["value"], "test1", "Failed to perform overwrite")
+    time.sleep(2)
+    self.assertEqual(helper.get("test"), {}, "Failed expire")
+    self.assertTrue(helper.put("test", "test2", 20000))
+    self.assertEqual(helper.get("test")["value"], "test2", "Store new value")
+
+if __name__ == "__main__":
+
+  main()
+
